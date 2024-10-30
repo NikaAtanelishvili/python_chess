e76d480d7a769f5505821d024fa67b49407c2e70
NikaAtanelishvili <atanelishvilinika@gmail.com>
2024-10-30 16:37:13 +0400

    feat: add basic King moving logic

--- src/piece.py
+++ src/piece.py
@@ -14,3 +14,15 @@ def _pos_to_cords(position):
     row = 8 - int(position[1])
 
     return row, col
+
+
+class King(Piece):
+    def is_valid_move(self, start, end, board):
+        start_row_pos, start_col_pos = _pos_to_cords(start)
+        end_row_pos, end_col_pos = _pos_to_cords(end)
+
+        # Check if the move is exactly one square away in any direction
+        row_diff = abs(end_row_pos - start_row_pos)
+        col_diff = abs(end_col_pos - start_col_pos)
+
+        return max(row_diff, col_diff) == 1
